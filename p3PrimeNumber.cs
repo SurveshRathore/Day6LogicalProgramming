3bba9f7dd883259c4ada713d0a79ee61849998c7
SurveshRathore <educatedevilboy@gmail.com>
2022-10-09 22:44:25 +0530

    3. Prime Number

---
+++ p3PrimeNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Program
+{
+    internal class p3PrimeNumber 
+    {
+        //method to check whether the number is a prime number or not
+        public void CheckPrimeNumber(int number)
+        {
+            //when the number is 2
+            if (number == 2)
+            {
+                //display that the number is a prime number
+                Console.WriteLine(number + " is the prime number.");
+            }
+            else
+            {
+                bool prime = false;
+
+                //loop to check whether the number is a prime number or not
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    else
+                    {
+                        prime = true;
+                    }
+                }
+                if (prime)
+                {
+                    //display that the number is a prime number
+                    Console.WriteLine(number + " is a prime number.");
+                }
+                else
+                {
+                    //display that the number is not a prime number
+                    Console.WriteLine(number + " is not a prime number.");
+                }
+            }
+            
+            
+        }
+
+    }
+}
